2db89d28ae273b7fe6c452b54f05a001acc6551c
samaybar <saybar@gmail.com>
2017-05-16 13:10:30 -0400

    added ability to export/import test environments and to modify when writing

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Runscope Environment Export/Import Scripts
 
-These two scripts are designed to facilitate copying of shared environments from Runscope.
+These two scripts are designed to facilitate copying of environments from Runscope.
 
 # Prerequisites
 To use these scripts, you need to get a Runscope Token.
@@ -25,9 +25,18 @@ You can also override default settings with various flags:
 `-c CONFIG_FILE_NAME`: overrides the default config file (don't include ".py")
 `-b BUCKET_ID`: overrides the bucket ID in config file
 `-e ENV_UUID`: overrides the Environment UUID in config file
-`-t RUNSCOPE_TOKEN`: overrides the Runscope token in config file
+`-t TEST_UUID`: if you specify a Test UUID, you can export a test environment (using ENV_UUID to go with the test)
+`-k RUNSCOPE_TOKEN`: overrides the Runscope token in config file
 
 ## Import
 usage: `python import_env.py <environmentname.json>`
+
+You can override default settings with various flags:
+`-c CONFIG_FILE_NAME`: overrides the default config file (don't include ".py")
+`-b BUCKET_ID`: overrides the bucket ID in config file
+`-e ENV_UUID`: overrides the Environment UUID in config file -- if you specify an environment UUID you also need to do the `-m` (modify) flag to indicate that you want to modify the enviornment
+`-t TEST_UUID`: if you specify a Test UUID, you can import an environment to a test
+`-k RUNSCOPE_TOKEN`: overrides the Runscope token in config file
+`-m`: add this flag to indicate that you are modifying the environment specified with the `-e ENV_UUID` flag
 
 
--- runscope_export_env.py
+++ runscope_export_env.py
@@ -13,11 +13,15 @@
 import requests
 import importlib
 import json
-#from runscope_config import *
 from optparse import OptionParser
 
-def export_env(m_bucket_key, m_env_id, m_runscope_token):
-	url = 'https://api.runscope.com/buckets/' + m_bucket_key + '/environments/' + m_env_id
+def export_env(m_bucket_key, m_env_id, m_runscope_token, m_test_id=False):
+	if (m_test_id):
+		url = 'https://api.runscope.com/buckets/' + m_bucket_key + '/tests/' + m_test_id + '/environments/' + m_env_id
+		print "exporting test"
+	else:
+		url = 'https://api.runscope.com/buckets/' + m_bucket_key + '/environments/' + m_env_id
+		print "exporting shared"
 	headers = dict(Authorization='Bearer ' + m_runscope_token)
 	r = requests.get(url, headers=headers)
 	if r.status_code == 200:
@@ -29,21 +33,19 @@ def export_env(m_bucket_key, m_env_id, m_runscope_token):
 		print 'Failure - ' + r.text
 
 ##------- Run the function above
+test_id = False
 parser = OptionParser()
 parser.add_option("-b", "--bucket", dest="bucket", help="bucket key for env to export", metavar="BUCKET")
 parser.add_option("-e", "--env", dest="uuid", help="env uuid for env to export", metavar="ENV_UUID")
 parser.add_option("-c", "--config", dest="configFile", help="name of config file", metavar="CONFIGFILE")
-parser.add_option("-t", "--token", dest="runscopeToken", help="Runscope API Token", metavar="RUNSCOPETOKEN")
-parser.add_option("-v", action='store_true', dest="verbose")#,help="increase output verbosity")
+parser.add_option("-k", "--token", dest="runscopeToken", help="Runscope API Token", metavar="RUNSCOPETOKEN")
+parser.add_option("-t", "--test", dest="testId", help="Runscope Test ID", metavar="TESTID")
+parser.add_option("-v", action='store_true', dest="verbose",help="increase output verbosity")
 (options, args) = parser.parse_args()
 if options.configFile:
-	configFileName = options.configFile
-	#importlib.import_module(configFileName, package=None)
+	configFileName = options.configFile	
 	mySettings = __import__(options.configFile)
-	#from eval(configFileName) import *
 else:
-	#importlib.import_module("runscope_config", package=None)
-	#from options.configFile import *
 	mySettings = __import__("runscope_config")
 master_bucket_key = mySettings.master_bucket_key
 master_env_id = mySettings.master_env_id
@@ -54,12 +56,16 @@ if options.uuid:
 	master_env_id = options.uuid
 if options.runscopeToken:
 	runscope_token = options.runscopeToken
+if options.testId:
+	test_id = options.testId
 if options.verbose:
 	print "==================================================="
 	print "ITEM          VALUE"
 	print "------------- -------------------------------------"
 	print "Bucket:       " + master_bucket_key
+	if options.testId:
+		print "Test ID:      " + test_id
 	print "Environment:  " + master_env_id
 	print "API Key:      " + runscope_token
 	print "==================================================="
-export_env(master_bucket_key, master_env_id, runscope_token)
+export_env(master_bucket_key, master_env_id, runscope_token, test_id)
